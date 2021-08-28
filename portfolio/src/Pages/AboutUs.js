41df84c7c810312d8bac1a3521563d439eba237f
Ryan Neil Angeles Vega <vegaryanneil@mgail.com>
2021-08-27 23:13:45 -0400

    About us section framework made

---
+++ portfolio/src/Pages/AboutUs.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import AboutSection from '../Components/AboutSection'
+const AboutUs = () => {
+    return (
+        <div>
+          <AboutSection/>  
+        </div>
+    )
+}
+
+export default AboutUs;
