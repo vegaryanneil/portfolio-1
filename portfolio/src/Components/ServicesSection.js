5cb37ef9f4049b55c957450cad03866a6da3b1d2
Ryan Neil Angeles Vega <vegaryanneil@mgail.com>
2021-09-01 23:10:29 -0400

    migrated styled components to another file, styled services section, added google font

--- portfolio/src/Components/ServicesSection.js
+++ portfolio/src/Components/ServicesSection.js
@@ -4,54 +4,78 @@ import diaphragm from '../img/diaphragm.svg';
 import money from '../img/money.svg';
 import teamwork from '../img/teamwork.svg';
 import home2 from '../img/home2.png';
+import styled from 'styled-components';
+import { About, Description, Image, Hide } from '../styles';
 
 const ServicesSection = () => {
     return (
-        <div className="services">
-            <div className="description">
+        <Services>
+            <Description>
                 <h2>High <span>quality</span> services</h2>
-                    <div className="cards">
-                        <div className="card">
+                    <Cards>
+                        <Card>
                             <div className="icon">
                                 <img src={clock} alt="clock" />
                                 <h3>Efficient</h3>
                             </div>
-                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum sunt ad, doloremque dolores rem dolore veritatis at amet veniam eveniet.</p>
-                        </div>
-                    </div>
-                    <div className="cards">
-                        <div className="card">
+                            <p>Lorem, ipsum dolor sit ame.</p>
+                        </Card>
+                        <Card>
                             <div className="icon">
                                 <img src={diaphragm} alt="diaphragm" />
                                 <h3>Efficient</h3>
                             </div>
-                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum sunt ad, doloremque dolores rem dolore veritatis at amet veniam eveniet.</p>
-                        </div>
-                    </div>
-                    <div className="cards">
-                        <div className="card">
+                            <p>Lorem, ipsum dolor sit amet consectetur </p>
+                        </Card>
+                        <Card>
                             <div className="icon">
                                 <img src={teamwork} alt="teamwork" />
                                 <h3>Efficient</h3>
                             </div>
-                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum sunt ad, doloremque dolores rem dolore veritatis at amet veniam eveniet.</p>
-                        </div>
-                    </div>
-                    <div className="cards">
-                        <div className="card">
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. </p>
+                        </Card>
+                        <Card>
                             <div className="icon">
                                 <img src={money} alt="money" />
                                 <h3>Efficient</h3>
                             </div>
-                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum sunt ad, doloremque dolores rem dolore veritatis at amet veniam eveniet.</p>
-                        </div>
-                    </div>
-                        <div className="image">
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. </p>
+                        </Card>
+                    </Cards>
+            </Description>    
+                        <Image>
                             <img src={home2} alt="home" />
-                        </div>
-            </div>    
-        </div>
+                        </Image>
+        </Services>
     )
 }
 
+const Services = styled(About)`
+h2 {
+    padding-bottom: 5rem;
+}
+p{
+    width: 70%;
+    padding: 2rem 0 4rem 0;
+}
+`;
+
+const Cards = styled.div`
+display: flex;
+flex-wrap: wrap;
+`;
+
+const Card = styled.div`
+flex-basis: 20rem;
+.icon {
+    display: flex;
+    align-items: center;
+h3 {
+    margin-left: 1rem;
+    background: white;
+    color: black;
+    padding: 1rem;
+}
+}
+`
 export default ServicesSection;
