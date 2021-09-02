0d5d29fb80ad8eaf3acbd78f1cbe4b1ab5c4e485
Ryan Neil Angeles Vega <vegaryanneil@mgail.com>
2021-09-02 16:01:45 -0400

    Set up styled nav, no function yet

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,10 +1,12 @@
 import GlobalStyle from "./Components/GlobalStyle";
+import Nav from "./Components/Nav";
 import AboutUs from "./Pages/AboutUs";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
+      <Nav />
       <AboutUs />
     </div>
   );
---
+++ portfolio/src/Components/Nav.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Nav = () => {
+    return (
+        <StyledNav>
+          <h1><a id="logo" href="home">Ryan Neil</a></h1>
+            <ul>
+                <li>
+                    <a href="About">About Me</a>
+                </li> 
+                <li>
+                    <a href="Work">My Work</a>
+                </li> 
+                <li>
+                    <a href="Contact">Contact Me</a>
+                </li> 
+            </ul>    
+        </StyledNav>
+    )
+}
+
+const StyledNav = styled.nav`
+min-height: 10vh;
+display: flex;
+margin: auto;
+justify-content: space-between;
+align-items: center;
+padding: 1rem 10rem;
+background-color: #282828;
+
+a{
+    color: white;
+    text-decoration: none;
+
+}
+ul{
+    display: flex;
+    list-style: none;
+
+}
+#logo {
+    font-size: 1.5rem;
+    font-weight: lighter;
+}
+li{
+    padding-left: 10rem;
+    position: relative;
+}
+`
+export default Nav
