79fad1369054fa19bdc05b228fccd0f45f1a234c
Ryan Neil Angeles Vega <vegaryanneil@mgail.com>
2021-09-01 22:40:07 -0400

    Styled images on about section

--- portfolio/src/Components/AboutSection.js
+++ portfolio/src/Components/AboutSection.js
@@ -4,22 +4,24 @@ import styled from 'styled-components';
 const AboutSection = () => {
     return (
         <About>
-            <div className="description">
+            <Description>
                 <div className="title">
-                    <div className="hide">
+                    <Hide>
                         <h2>We work to make</h2>
-                    </div>
-                    <div className="hide">
+                    </Hide>
+                    <Hide>
                         <h2>your <span>dreams</span> come</h2>
-                    </div>
-                    <div className="hide">
+                    </Hide>
+                    <Hide>
                         <h2>true.</h2>
-                    </div>
+                    </Hide>
                 </div>
                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste, exercitationem qui rem ullam est molestiae obcaecati ad repudiandae illo inventore.</p>
                 <button>Contact Us</button>
-            </div>
+            </Description>
+            <Image>
             <img src={home1} alt="guy with camera" />
+            </Image>
         </About>
     )
 }
@@ -32,5 +34,26 @@ justify-content: space-between;
 padding: 5rem 10rem;
 color: white;
 `;
+
+const Description = styled.div`
+flex: 1;
+padding-right: 5rem;
+h2 {
+    font-weight: lighter;
+}
+`;
+
+const Image = styled.div`
+flex: 1;
+overflow: hidden;
+img {
+    width: 100%;
+    height: 80vh;
+    object-fit: cover;
+}
+`
+const Hide = styled.div`
+overflow: hidden;
+`
 
 export default AboutSection;
