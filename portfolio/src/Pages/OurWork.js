e684d948b7abdc1cc46a08871a8a23700cff9ff9
Ryan Neil Angeles Vega <vegaryanneil@mgail.com>
2021-09-06 16:08:24 -0400

    Added animation to work. edited page transition to make it snappier

--- portfolio/src/Pages/OurWork.js
+++ portfolio/src/Pages/OurWork.js
@@ -5,32 +5,40 @@ import athlete from '../img/athlete-small.png';
 import theracer from '../img/theracer-small.png';
 import goodtimes from '../img/goodtimes-small.png';
 import { motion } from 'framer-motion'
-import { pageAnimation } from './Animation'
+import { sliderContainer,fade, photoAnim, pageAnimation, lineAnim, slider } from './Animation'
 
 const OurWork = () => {
     return (
         <Work variants={pageAnimation} initial="hidden" animate="show" exit="exit" style={{ background: "#fff"}}>
-            <Movie>
-             <h2>The Athlete</h2>
-             <div className="line"></div>
-             <Link to="/work/the-athlete">
-             <img src={athlete} alt="athlete" />
-             </Link>
-            </Movie>
-            <Movie>
-             <h2>The Racer</h2>
-             <div className="line"></div>
-             <Link to="/work/the-racer">
-             <img src={theracer} alt="racer" />
-             </Link>
-            </Movie>
-            <Movie>
-             <h2>The Good Times</h2>
-             <div className="line"></div>
-             <Link to="/work/the-goodtimes">
-             <img src={goodtimes} alt="good times" />
-             </Link>
-            </Movie>
+            <motion.div variants={sliderContainer}>
+                <Frame1 variants={slider}></Frame1>
+                <Frame2 variants={slider}></Frame2>
+                <Frame3 variants={slider}></Frame3>
+                <Frame4 variants={slider}></Frame4>
+            </motion.div>
+                <Movie>
+                <motion.h2 variants={fade}>The Athlete</motion.h2>
+                <motion.div variants={lineAnim} className="line"></motion.div>
+                <Link to="/work/the-athlete">
+                    <Hide>
+                        <motion.img variants={photoAnim} src={athlete} alt="athlete" />
+                    </Hide>
+                </Link>
+                </Movie>
+                <Movie>
+                <h2>The Racer</h2>
+                <div className="line"></div>
+                <Link to="/work/the-racer">
+                <img src={theracer} alt="racer" />
+                </Link>
+                </Movie>
+                <Movie>
+                <h2>The Good Times</h2>
+                <div className="line"></div>
+                <Link to="/work/the-goodtimes">
+                <img src={goodtimes} alt="good times" />
+                </Link>
+                </Movie>
         </Work>
     )
 }
@@ -57,5 +65,28 @@ img {
     height: 70vh;
     object-fit: cover;
 }
+`
+
+const Hide = styled.div`
+overflow: hidden;
+`
+
+const Frame1 = styled(motion.div)`
+position: fixed;
+left: 0;
+top: 10%;
+width: 100%;
+height: 100vh;
+background: #fffebf;
+z-index: 2;
+`
+const Frame2 = styled(Frame1)`
+background: #ff8efb;
+`
+const Frame3 = styled(Frame1)`
+background: #8ed2ff;
+`
+const Frame4 = styled(Frame1)`
+background: #8effa0;
 `
 export default OurWork
