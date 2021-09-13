b41d2609b59311971ca5ffa7a87527d8aad532a7
Ryan Neil Angeles Vega <vegaryanneil@mgail.com>
2021-09-12 22:33:36 -0400

    Added a hiding animation for service and faq

--- portfolio/src/Components/ServicesSection.js
+++ portfolio/src/Components/ServicesSection.js
@@ -6,10 +6,13 @@ import teamwork from '../img/teamwork.svg';
 import home2 from '../img/home2.png';
 import styled from 'styled-components';
 import { About, Description, Image} from '../styles';
+import { useScroll } from './useScroll';
+import {fade} from "../Pages/Animation"
 
 const ServicesSection = () => {
+    const [element, controls] = useScroll();
     return (
-        <Services>
+        <Services variants={fade} ref={element} animate={controls} intial="hidden">
             <Description>
                 <h2>High <span>quality</span> services</h2>
                     <Cards>
